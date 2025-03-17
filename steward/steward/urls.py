175b3e5769389d3d84e8667a75d8eb716eab23d0
shustada <adamshuster1@gmail.com>
2025-03-16 20:11:21 -0400

    login/logout

--- steward/steward/urls.py
+++ steward/steward/urls.py
@@ -16,11 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 from steward import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),  # Built-in authentication URLs
     path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', views.home, name='home'),
 ]
